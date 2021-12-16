ce8eaf617fd3d87ed5194b510245b8d15a8cc592
Arundhati <arundhatiraicar12@gmail.com>
2021-12-16 12:47:19 +0530

    Header with button

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import react from 'react';
+import Header from './Header';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Fitness
-        </h1>
-        
+        <h1>Welcome to My Fitness</h1>
+       <Header />
       </header>
-    </div>
+   </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      
+      <button >
+       Mode
+      </button>
+    </header>
+  );
+}
+
+export default Header;
