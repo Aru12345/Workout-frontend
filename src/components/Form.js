c37cbb8d27bb4cf32908fccb2c71c0fc0238d291
Arundhati <arundhatiraicar12@gmail.com>
2021-12-22 20:36:17 +0530

    Form created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 function Form(){
+
+    const[formData,setFormData]=useState({
+        name:'',
+        equip:'',
+        duration:'',
+        exercises:'',
+        notes:''
+    })
+    
     return(
-        <>
-        <h1>Form</h1>
-        </>
+       <form>
+           <h2>Build your Workout</h2>
+           <label>Name:</label>
+           <input type="text" name="name" aria-label="name" value={formData.name}  ></input>
+           <label>Equipment:</label>
+           <input type="text" name="equipment" aria-label="equipment"  value={formData.equip}></input>
+           <label>Duration:</label>
+           <input type="text" name="duration" aria-label="duration" value={formData.duration}></input>
+           <label>Exercises:</label>
+           <input type="text" name="exercise" aria-label="exercise" value={formData.exercises}></input>
+           <label>Notes:</label>
+           <input type="text" name="note" aria-label="note" value={formData.notes}></input>
+           <input type="submit" />
+          
+       </form>
     )
 }
 export default Form;
--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 function PlanCard({plan}){
-    const { id, name, duration,exercises,equipments,note} = plan;
+    const { id, name,equip, duration,exercises,notes} = plan;
     return(
      
         <p className="plancard">
             <h3>Name-{name}</h3>
-            <h3>Equipment-{equipments}</h3>
+            <h3>Equipment-{equip}</h3>
             <h3>Duration-{duration}</h3>
             <h3>Exercise-{exercises}</h3>
-            <h3>Note- {note}</h3>
+            <h3>Note- {notes}</h3>
 
 
         </p>
