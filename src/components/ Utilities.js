4d141ddc065389f68f95e0331f61562c127c0316
Arundhati <arundhatiraicar12@gmail.com>
2021-12-24 23:09:17 +0530

    Iframes embeded

--- src/components/ Utilities.js
+++ src/components/ Utilities.js
@@ -6,7 +6,10 @@ function  Utilities(){
      <h2>utlities</h2>
      <iframe src=" https://www.mealpro.net/bmi/?color=34a6b0 " width={500} height={350}></iframe>
      <iframe id="sc-protiene_intake_cal" width=" 500" height="560" allowtransparency="true" src="https://www.stylecraze.com/tools/protein-intake-calculator/?embed_id=1" frameborder="0" allowfullscreen=""></iframe> 
+     <iframe src='https://www.stemjar.com/wp-admin/body_fat_calculator_index.html'  width={500} height={350} id='showiframe1'></iframe>
+     <iframe src="https://www.mealpro.net/calorie/?color=34a6b0" frameborder="0" width={700} height={550} ></iframe>
      </>
  )
 }
 export default Utilities;
+ 
