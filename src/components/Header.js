2276fd88141b8f6ed3c505c7a96a0e45336ab84d
Arundhati <arundhatiraicar12@gmail.com>
2021-12-28 13:16:42 +0530

    Navbar and Navlinks added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,68 @@
 import React from "react";
 import styled from "styled-components";
+import { NavLink} from "react-router-dom"
 //import styled from "styled-components";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function Header(props) {
   return (
-    <HeaderContainer>
-      
-      <h1>Welcome to My Fitness</h1>
-      
-    </HeaderContainer>
+    <>
+    <h1>Welcome to My Fitness</h1>
+    <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/exercises"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/plans/new"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Plans
+      </NavLink>
+      <NavLink
+        to="/utilities/new"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Utilities
+      </NavLink>
+
+      </>
   );
 }
 
 
 export default Header;
 
-const HeaderContainer=styled.div`
-background-image: linear-gradient(to right, purple, blue);
-  min-height: 14vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(8px + 2vmin);
-  color: white;
-  font-family: monospace;
- font-style: italic;
- 
- width:100%
- 
- top:0px;
- right:0px;
 
- left:0px;
- 
- 
- 
-`
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,9 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 function Home(){
     return(
         <>
-        <div className="homeCards">
-        <p className="exCard">
-            <img src="https://i.ytimg.com/vi/5swqLti2KUg/maxresdefault.jpg"></img>
-            <button>Search Exercises</button>
-        </p>
-        <p className="plCard">
-            <img src="http://gettinmyhealthyon.com/wp-content/uploads/2015/09/Weekly-Workout-Schedule.jpg"></img>
-            <button>Build your workout</button>
-        </p>
-        <p className="ulCard">
-            <img src="https://image.winudf.com/v2/image1/Y29tLmlhcHAuZ3ltLnV0aWxpdGllc19zY3JlZW5fMF8xNTQ1NzE2NDExXzAzNg/screen-0.jpg?fakeurl=1&type=.jpg"></img>
-            <button>Use Utilities</button>
-        </p>
-
-        </div>
-       <h1>Home</h1>
+        <h1>Home</h1>
         </>
     )
 }
